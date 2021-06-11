fc5c2a8f30028c37133aa2631b12bb3db53c5c22
Pritomrit Bora <pritomrit@MBP-de-mac.home>
2021-06-11 17:42:19 +0200

    dynamic save weight parameter

--- src/detector.c
+++ src/detector.c
@@ -391,7 +391,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
 
         //if (i % 1000 == 0 || (i < 1000 && i % 100 == 0)) {
         //if (i % 100 == 0) {
-        if (iteration >= (iter_save + 1000) || iteration % 1000 == 0) {
+        if (iteration >= (iter_save + save_w) || iteration % save_w == 0) {
             iter_save = iteration;
 #ifdef GPU
             if (ngpus != 1) sync_nets(nets, ngpus, 0);
